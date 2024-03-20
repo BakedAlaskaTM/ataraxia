9f15d95c08eca6fa63665892eee02775b067dbd0
Devin Shen <ds21616@my.westlake.school.nz>
2024-03-21 12:01:28 +1300

    Basic first map done.

--- maps/Testing Tiles.tsx
+++ maps/Testing Tiles.tsx
@@ -1,10 +1,28 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.9" tiledversion="1.9.2" name="Testing Tiles" tilewidth="64" tileheight="64" tilecount="2" columns="0">
+<tileset version="1.9" tiledversion="1.9.2" name="Testing Tiles" tilewidth="128" tileheight="128" tilecount="8" columns="0">
  <grid orientation="orthogonal" width="1" height="1"/>
  <tile id="0">
   <image width="16" height="16" source="../assets/Tiles/Testing/BlackSquare.png"/>
  </tile>
  <tile id="1">
   <image width="64" height="64" source="../assets/Tiles/Testing/Statue.png"/>
+ </tile>
+ <tile id="2">
+  <image width="128" height="128" source="../assets/Tiles/Testing/House.png"/>
+ </tile>
+ <tile id="3">
+  <image width="16" height="16" source="../assets/Tiles/Testing/DarkStone.png"/>
+ </tile>
+ <tile id="4">
+  <image width="16" height="16" source="../assets/Tiles/Testing/Dirt.png"/>
+ </tile>
+ <tile id="5">
+  <image width="16" height="16" source="../assets/Tiles/Testing/Grass.png"/>
+ </tile>
+ <tile id="6">
+  <image width="16" height="16" source="../assets/Tiles/Testing/Stone.png"/>
+ </tile>
+ <tile id="7">
+  <image width="16" height="16" source="../assets/Tiles/Testing/HalfDirt.png"/>
  </tile>
 </tileset>
